2c0743af7dddd4630d91a960c19902a6785d84b9
AL7MIDY <45107747+AL7MIDY@users.noreply.github.com>
2018-12-23 01:01:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,7 +24,24 @@ client.on('message', message => {
   }
 });
 
+client.on('message', message => {
+  // Voice only works in guilds, if the message does not come from a guild,
+  // we ignore it
+  if (!message.guild) return;
 
+  if (message.content === 'تعال') {
+    		        if (message.author.id != ownerid) return;
+
+    // Only try to join the sender's voice channel if they are in one themselves
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel.join()
+        .then(connection => { // Connection is an instance of VoiceConnection
+        })
+        .catch(console.log);
+    } else {
+    }
+  }
+});
 
 
 client.login(process.env.BOT_TOKEN);
