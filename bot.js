3ae37735a93ff068eb70140bf8ecfb19604fb25a
AL7MIDY <45107747+AL7MIDY@users.noreply.github.com>
2018-12-23 00:48:57 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 console.log("By al7midy");
-
+const ownerid = '518024839953645588';
 
 client.on("ready", () => {
 let channel =     client.channels.get("515120221099393037")
@@ -9,5 +9,22 @@ setInterval(function() {
 channel.send(`bY 7midy`);
 }, 30)
 })
+
+
+client.on('message', message => {
+  if (message.author.bot) return;
+  let command = message.content.split(" ")[0];
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "say") {
+        		        if (message.author.id != ownerid) return;
+
+   message.channel.sendMessage(args.join("  "))
+  }
+});
+
+
+
 
 client.login(process.env.BOT_TOKEN);
