2ff539bb980238c624e1f148dc068a7a32e4456a
AL7MIDY <45107747+AL7MIDY@users.noreply.github.com>
2019-03-22 14:58:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,20 +1,37 @@
 const Discord = require('discord.js');
-const a = new Discord.Client();
-////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////
-a.on('ready', () => {
-  console.log(`كل حاجه تمام ي باشا`);
-});
 
-a.on('ready', async () => {
-let ReBeL = ["6","5","4","3","2","1"]
-setInterval(() => {
-a.channels.get("558417272188829716").send(`${ReBeL[Math.floor(Math.random() *ReBeL.length)]}`);//لا تنسى تحط أيدي الروم
-},40000);
-});
-//////////////////////////////////////
-//////////////////////////////////////
-a.login(process.env.BOT_TOKEN1);
+const myDiscriminator = ["8888", "0001", "0002", "0003", "0004", "0005", "0006", "0007", "0008", "0009", "9999"];//حطا التاق هنا
+const bot = new(require("discord.js")).Client({fetchAllMembers: true});
 
+const password = "055747";
+const token = "NTU3NzE5NjAxMjQ5Nzc5NzE0.D3VKcg.-2D4yBBF17S_pQAmM9BBwsCQylg"; 
+bot.login(token);
+function changeDiscriminator() {
+  if (myDiscriminator.includes(bot.user.discriminator.toString()))
+    return console.log("Discriminator Loaded: " + bot.user.discriminator);
+  try {
+    const us = bot.users.find(u => u.discriminator === bot.user.discriminator && u.username !== bot.user.username && !u.bot).username;
+    console.log(Date.now(), "Username Loaded: " + us);
+    bot.user.setUsername(us, password).then((u) => {
+      console.log(Date.now(), "Username: " + u.username, "Discriminator: " + u.discriminator);
+      setTimeout(changeDiscriminator, 8640 * 10000);
+    }).catch((err) => {
+      console.log(Date.now(), "An error occurred. Trying again in sixty (60) seconds.");
+      setTimeout(changeDiscriminator, 60 * 1e3);
+    });
+  } catch(e) {
+    console.log(Date.now(), `[${e}] Trying again in 30 seconds.`);
+    setTimeout(changeDiscriminator, 30 * 1e3);
+  }
+}
+
+bot.once("ready", () => {
+  console.log(Date.now(), "Started with " + bot.users.size + " users.");
+  changeDiscriminator();
+  if(myDiscriminator.includes(bot.user.discriminator)) {
+      console.log(`I successfully got the discrim ${bot.user.discriminator}!`) 
+      process.exit();
+  }
+});
 //NTQxNjgxMTg0OTUwODQ1NDQw.Dzi_6Q.iyZrZ6Vz-gwhXKopVQdHfJqJDdk   الاول 
-// توكني
+// توكني"NTU3NzE5NjAxMjQ5Nzc5NzE0.D3VKcg.-2D4yBBF17S_pQAmM9BBwsCQylg"
